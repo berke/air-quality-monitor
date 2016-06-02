ed39ae88f68ac22c97c06c2731f05c71053b09c5
Berke Durak <berke.durak@gmail.com>
2016-06-02 08:41:00 -0700

    Remove dependency on Lwt Camlp4 syntax extension.

--- dylos_logger.ml
+++ dylos_logger.ml
@@ -4,71 +4,71 @@
 
    Tool for logging the output of Dylos air quality monitors
 
-   Copyright (C) 2014 Berke Durak
+   Copyright (C) 2014-2016 Berke Durak
 
    Author: Berke Durak <berke.durak@gmail.com>
 
-*)
+ *)
 
 open Lwt
 open Lwt_log
 open Printf
 
 module Opt =
-struct
-  let port = ref "/dev/ttyUSB0"
-  let baud = ref 9600
-  let log_file = ref "dylos.log"
-  let session_file : string option ref = ref None
-  let heartbeat_delay = ref 600.0
-end
+  struct
+    let port = ref "/dev/ttyUSB0"
+    let baud = ref 9600
+    let log_file = ref "dylos.log"
+    let session_file : string option ref = ref None
+    let heartbeat_delay = ref 600.0
+  end
 
 module Spec =
-struct
-  open Arg
-  open Opt
+  struct
+    open Arg
+    open Opt
 
-  let specs =
-    align @@
-    [
-      "--port",
-      Set_string port,
-      "<path> Set serial port";
+    let specs =
+      align @@
+	[
+	  "--port",
+	  Set_string port,
+	  "<path> Set serial port";
 
-      "--baud",
-      Set_int baud,
-      "<rate> Set port speed";
+	  "--baud",
+	  Set_int baud,
+	  "<rate> Set port speed";
 
-      "--log-file",
-      Set_string log_file,
-      "<path> Set log file";
+	  "--log-file",
+	  Set_string log_file,
+	  "<path> Set log file";
 
-      "--session-file",
-      String(fun u -> session_file := Some u),
-      "<path> Use session file to provide a persistent serial number";
+	  "--session-file",
+	  String(fun u -> session_file := Some u),
+	  "<path> Use session file to provide a persistent serial number";
 
-      "--heartbeat-delay",
-      Set_float heartbeat_delay,
-      "<float> Interval between heartbeats";
-    ]
-end
+	  "--heartbeat-delay",
+	  Set_float heartbeat_delay,
+	  "<float> Interval between heartbeats";
+	]
+  end
 
 let line_splitter ?(limit=128) ic =
   let b = Buffer.create 128 in
   let rec loop () =
-    lwt c = Lwt_io.read_char ic in
+    Lwt_io.read_char ic >>= fun c ->
     match c with
     | '\n' -> loop ()
     | '\r' ->
-      let u = Buffer.contents b in
-      Buffer.clear b;
-      debug_f "<< %S" u >>
-      return u
+       let u = Buffer.contents b in
+       Buffer.clear b;
+       debug_f "<< %S" u >>= fun () ->
+       return u
     | c when Buffer.length b < limit ->
-      Buffer.add_char b c;
-      loop ()
+       Buffer.add_char b c;
+       loop ()
     | _ ->
-      loop ()
+       loop ()
   in
   loop
 
@@ -110,10 +110,10 @@ let make_raw tio =
   } 
 
 let serial_setup port baud =
-  lwt fd = Lwt_unix.openfile port [Unix.O_RDWR;Unix.O_NONBLOCK] 0o644 in
-  lwt ta = Lwt_unix.tcgetattr fd in
+  Lwt_unix.openfile port [Unix.O_RDWR;Unix.O_NONBLOCK] 0o644 >>= fun fd ->
+  Lwt_unix.tcgetattr fd >>= fun ta ->
   let ta' = set_baud baud @@ make_raw ta in
-  lwt _ = Lwt_unix.tcsetattr fd Unix.TCSANOW ta' in
+  Lwt_unix.tcsetattr fd Unix.TCSANOW ta' >>= fun _ ->
   let r_ch = Lwt_io.of_fd ~mode:Lwt_io.input fd
   and w_ch = Lwt_io.of_fd ~mode:Lwt_io.output fd
   in
@@ -122,134 +122,130 @@ let serial_setup port baud =
 let main () =
   let open Opt in
 
-  lwt serial_r_ch, serial_w_ch = serial_setup !port !baud in
+  serial_setup !port !baud >>= fun (serial_r_ch, serial_w_ch) ->
 
   let drain timeout =
     let drainer =
-      while_lwt true do
-        lwt _ = Lwt_io.read_char serial_r_ch in
-        return ()
-      done
+      let rec loop () = Lwt_io.read_char serial_r_ch >>= fun c -> loop () in
+      loop ()
     in
     let sleeper =
-      lwt () = Lwt_unix.sleep timeout in
+      Lwt_unix.sleep timeout >>= fun () ->
       cancel drainer;
       return ()
     in
-    try_lwt
-      join [sleeper; drainer]
-    with
-    | Canceled -> return ()
+    try_bind
+      (fun () -> join [sleeper; drainer])
+      (fun () -> return ())
+      (function
+	| Canceled -> return ()
+	| e -> fail e)
   in
 
-  lwt () = drain 1.0 in
+  drain 1.0 >>= fun () ->
 
   let next_line = line_splitter serial_r_ch in
 
   let t0 = Unix.gettimeofday () in
   let counter = ref 0 in
 
-  lwt session =
-    match !Opt.session_file with
-    | None -> return 0
-    | Some fn ->
-      lwt session = 
-        catch
-          (fun () ->
-             lwt st = Lwt_unix.stat fn in
-             if st.Unix.st_kind = Unix.S_REG then
-               Lwt_io.with_file
-                 ~perm:0o755
-                 ~mode:Lwt_io.input
-                 fn
-                 (fun ic ->
-                    lwt u = Lwt_io.read_line ic in
-                    return @@ int_of_string u) 
-             else
-               (
-                 info_f "Session file %S is not regular" fn >>
-                 return 0
-               )
-          )
-          (fun e ->
-             error_f "Cannot read from session file %S: %s"
-               fn
-               (Printexc.to_string e) >>
-             return 0)
-      in
+  (match !Opt.session_file with
+   | None -> return 0
+   | Some fn ->
+      (catch
+	 (fun () ->
+	  Lwt_unix.stat fn >>= fun st ->
+	  if st.Unix.st_kind = Unix.S_REG then
+	    Lwt_io.with_file
+	      ~perm:0o755
+	      ~mode:Lwt_io.input
+	      fn
+	      (fun ic ->
+	       Lwt_io.read_line ic >>= fun u ->
+	       return @@ int_of_string u) 
+	  else
+	    (
+	      info_f "Session file %S is not regular" fn >>= fun () ->
+	      return 0
+	    )
+	 )
+	 (fun e ->
+	  error_f "Cannot read from session file %S: %s"
+		  fn
+		  (Printexc.to_string e) >>= fun () ->
+	  return 0)) >>= fun session ->
       let session' = session + 1 in
       Lwt_io.with_file
-        ~flags:Unix.([O_DSYNC;O_TRUNC;O_CREAT;O_WRONLY])
-        ~perm:0o755
-        ~mode:Lwt_io.output
-        fn
-        (fun oc -> Lwt_io.fprintf oc "%d" session') >>
+	~flags:Unix.([O_DSYNC;O_TRUNC;O_CREAT;O_WRONLY])
+	~perm:0o755
+	~mode:Lwt_io.output
+	fn
+	(fun oc -> Lwt_io.fprintf oc "%d" session') >>= fun () ->
       return session
-  in
-
+  ) >>= fun session ->
+  
   let rec heartbeat_task () =
     let dt = Unix.gettimeofday () -. t0 in
     (
       if dt > 0.0 then
-        info_f "Session %d: %d readings in %f s (%f reading / s)"
-          session
-          !counter
-          dt
-          (float !counter /. dt)
+	info_f "Session %d: %d readings in %f s (%f reading / s)"
+	       session
+	       !counter
+	       dt
+	       (float !counter /. dt)
       else
-        info_f "Starting"
-    ) >>
+	info_f "Starting"
+    ) >>= fun () ->
     Lwt_unix.sleep !heartbeat_delay >>=
-    heartbeat_task
+      heartbeat_task
   in
 
   let main oc =
     let rec loop () =
-      lwt u = next_line () in
+      next_line () >>= fun u ->
       (
-        match
-          (
+	match
+	  (
 
-            try
-              Scanf.sscanf u "%d,%d" (fun x y -> Some(x,y))
-            with
-            | _ -> None
-          )
-        with
-        | None -> error_f "Cannot parse %S" u
-        | Some(x,y) ->
-          incr counter;
-          let open Unix in
-          let t = gettimeofday () in
-          let tm = localtime t in
-          Lwt_io.fprintf oc
-            "%5d %8d %04d-%02d-%02d %02d:%02d:%02d %16.4f %5d %5d\n"
-            session !counter
-            (tm.tm_year + 1900)
-            (tm.tm_mon + 1)
-            tm.tm_mday
-            tm.tm_hour
-            tm.tm_min
-            tm.tm_sec
-            t
-            x
-            y
-            >>
-          Lwt_io.flush oc
+	    try
+	      Scanf.sscanf u "%d,%d" (fun x y -> Some(x,y))
+	    with
+	    | _ -> None
+	  )
+	with
+	| None -> error_f "Cannot parse %S" u
+	| Some(x,y) ->
+	   incr counter;
+	   let open Unix in
+	   let t = gettimeofday () in
+	   let tm = localtime t in
+	   Lwt_io.fprintf oc
+			  "%5d %8d %04d-%02d-%02d %02d:%02d:%02d %16.4f %5d %5d\n"
+			  session !counter
+			  (tm.tm_year + 1900)
+			  (tm.tm_mon + 1)
+			  tm.tm_mday
+			  tm.tm_hour
+			  tm.tm_min
+			  tm.tm_sec
+			  t
+			  x
+			  y
+	   >>= fun () ->
+	   Lwt_io.flush oc
       )
-      >>
-      loop ()
+      >>= loop
     in
     loop ()
   in
 
   heartbeat_task () <&>
-  Lwt_io.with_file
-    ~flags:Unix.([O_DSYNC;O_APPEND;O_CREAT;O_WRONLY])
-    ~perm:0o755
-    ~mode:Lwt_io.output
-    !Opt.log_file
-    main
+    Lwt_io.with_file
+      ~flags:Unix.([O_DSYNC;O_APPEND;O_CREAT;O_WRONLY])
+      ~perm:0o755
+      ~mode:Lwt_io.output
+      !Opt.log_file
+      main
 
 let _ =
   (
@@ -260,10 +256,8 @@ let _ =
         (Printf.sprintf "Usage: %s [options]" Sys.argv.(0));
     with
     | Arg.Bad b ->
-      Printf.fprintf Pervasives.stderr "%s\n%s\n"
-        b
-        (Arg.usage_string Spec.specs "Usage:")
+       Printf.fprintf Pervasives.stderr "%s\n%s\n"
+		      b
+		      (Arg.usage_string Spec.specs "Usage:")
   );
   Lwt_main.run @@ main ()
-
-(* vim:set tw=80 cc=80 sw=2 ts=2 expandtab: *)
